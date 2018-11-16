389e466579b8d5056758e2c4adc7ab0bd81acebb
Oleksandr Khitko <oleksandr@taotesting.com>
2018-11-16 11:41:00 +0100

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -787,6 +787,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('12.3.1');
         }
 
-        $this->skip('12.3.1', '12.3.2');
+        $this->skip('12.3.1', '12.4.0');
     }
 }
